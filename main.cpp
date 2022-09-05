aa63357479cb78949773e619c73e67eacfcd396d
satrioramadhan <112855964+satrioramadhan@users.noreply.github.com>
2022-09-05 15:22:46 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Satrio Ramadhan Wicaksono" << endl;
+    cout << "A11.2022.14150" << endl;
+    cout << "Jl. Roro Jonggrang Dalam I No.29, Manyaran, Kec. Semarang Barat, Kota Semarang" << endl;
+    cout << "SMK Negeri 1 Adowerna" << endl;
+    return 0;
+}
