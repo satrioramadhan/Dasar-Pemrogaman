1a8040ff8c5f3fb2d1cabc5b28703838a0edc3dc
satrioramadhan <112855964+satrioramadhan@users.noreply.github.com>
2022-09-12 15:03:40 +0700

    Add files via upload

---
+++ satrio project pertemuan 3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "satrio" << endl;
+    /*
+    printf ("test")
+    */
+    int u = 18;
+    float t = 177.5;
+    string ub = "XL";
+    cout << "umur saya: " << u;
+    cout << endl;
+    cout << "tinggi badan: " << t;
+    cout << "\nUkuran baju: " << ub;
+
+    return 0;
+}
